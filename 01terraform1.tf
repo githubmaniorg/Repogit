6bb2e668eef84adb03a48b9d570c4a6b900e2c17
manikantanukala <107236759+manikantanukala@users.noreply.github.com>
2022-09-13 17:26:41 +0530

    Update 01terraform1.tf

--- 01terraform1.tf
+++ 01terraform1.tf
@@ -1,14 +1,14 @@
 provider "aws" {
 
 region = "ap-south-1"
-access_key = "AKIA3KLLWGXHMX5NAHF7"
-secret_key = "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"
+access_key = "AKIA3KLLWGXHL3G74FGD"
+secret_key = "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"
 }
 
 resource "aws_instance" "AWSEC2Instance"{
     ami = "ami-05c8ca4485f8b138a"
     instance_type = "t2.micro" 
-    security_groups = ["launch-wizard-2"]
+    security_groups = ["launch-wizard-14"]
     key_name= "manikantadevops.pem"
     tags = {
         Name = "Redhat server by TerraForm"
